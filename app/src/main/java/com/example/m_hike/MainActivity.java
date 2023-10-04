273e89103196839ca8a08a2f4c5d4db1b74a2c6f
Nguyễn Minh Triết <minhtriet241203@gmail.com>
2023-10-04 16:12:48 +0700

    Remove title bar

--- app/src/main/java/com/example/m_hike/MainActivity.java
+++ app/src/main/java/com/example/m_hike/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.m_hike;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Hiding the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
     }
 }
