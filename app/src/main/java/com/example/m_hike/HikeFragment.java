703a391b9d1564b6eaabeca621439221eccfaa4c
Nguyễn Minh Triết <minhtriet241203@gmail.com>
2023-10-09 01:04:14 +0700

    Working on create page

--- app/src/main/java/com/example/m_hike/HikeFragment.java
+++ app/src/main/java/com/example/m_hike/HikeFragment.java
@@ -2,20 +2,31 @@ package com.example.m_hike;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.m_hike.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HikeFragment extends Fragment {
-
-    Helper helper = new Helper(getParentFragmentManager());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hike, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
