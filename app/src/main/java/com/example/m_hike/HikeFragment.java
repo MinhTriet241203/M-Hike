b2f4237f6600094727f1c170417aebaad1a6d03a
Nguyễn Minh Triết <minhtriet241203@gmail.com>
2023-10-19 16:28:05 +0700

    Done designing

--- app/src/main/java/com/example/m_hike/HikeFragment.java
+++ app/src/main/java/com/example/m_hike/HikeFragment.java
@@ -12,18 +12,25 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class HikeFragment extends Fragment {
 
     Spinner difficultySpinner;
-    TextInputLayout descriptionInputLayout;
+    TextInputLayout otherInputLayout, durationInputLayout;
+    TextInputEditText durationInput;
+    CheckBox otherCB;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,8 +44,24 @@ public class HikeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         difficultySpinner = requireView().findViewById(R.id.difficultySpinner);
+        otherCB = requireView().findViewById(R.id.otherCB);
+        otherInputLayout = requireView().findViewById(R.id.otherEquipmentInputLayout);
+        durationInput = requireView().findViewById(R.id.durationInput);
+        durationInputLayout = requireView().findViewById(R.id.durationInputLayout);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(requireContext().getApplicationContext(), R.layout.drop_down_item, getResources().getStringArray(R.array.difficulty));
         difficultySpinner.setAdapter(arrayAdapter);
+
+        otherCB.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked) {
+                otherInputLayout.setVisibility(View.VISIBLE);
+            } else {
+                otherInputLayout.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private boolean isEmpty(EditText input) {
+        return input.getText().toString().trim().length() == 0;
     }
 }
