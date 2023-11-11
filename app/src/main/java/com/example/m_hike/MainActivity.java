2941823d39b6c634fb54cc657e9f5bf2b35fcc61
MinhTriet241203 <minhtriet241203@gmail.com>
2023-11-11 10:40:50 +0700

    commit

--- app/src/main/java/com/example/m_hike/MainActivity.java
+++ app/src/main/java/com/example/m_hike/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.m_hike;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.m_hike.databinding.ActivityMainBinding;
 
@@ -112,5 +114,13 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
         dialog.getWindow().setGravity(Gravity.BOTTOM);
         dialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.);
+        assert fragment != null;
+        fragment.onActivityResult(requestCode, resultCode, data);
     }
 }
